eab1aa87cc6a70f361d16a9dd6e032369047881a
mmanna3 <mmmannna3@gmail.com>
2023-06-14 21:31:05 -0300

    Palabra secreta redirige a mazo-de-canciones

--- app/components/body.tsx
+++ app/components/body.tsx
@@ -1,9 +1,9 @@
 "use client";
-import { useState } from "react";
-// import MazoDeCanciones from "../mazoDeCanciones/MazoDeCanciones";
-// import BotonNavegacion from "./BotonBlog/BotonBlog";
 import ImagenMandarina from "@/components/imagenMandarina";
 import InputSecreto from "@/components/inputSecreto";
+import { RedirectType } from "next/dist/client/components/redirect";
+import { redirect } from "next/navigation";
+import { useMemo, useState } from "react";
 
 const Body = () => {
   const [mandarinaHeridaEsVisible, mostrarMandarinaHerida] = useState(false);
@@ -16,24 +16,21 @@ const Body = () => {
     mostrarMazo(true);
   };
 
+  useMemo(() => {
+    if (mazoEsVisible) redirect("/mazo-de-canciones", RedirectType.push);
+  }, [mazoEsVisible]);
+
   return (
     <div className="flex flex-col items-center justify-center h-[85vh]">
-      {!mazoEsVisible ? (
-        <>
-          <ImagenMandarina
-            mandarinaHeridaEsVisible={mandarinaHeridaEsVisible}
-            cuandoLaApreten={() => apretarLaMandarina((prev) => !prev)}
-          />
-          {apretoLaMandarina && (
-            <InputSecreto
-              hayError={hayError}
-              escribioLaCorrecta={escribioLaCorrecta}
-            />
-          )}
-        </>
-      ) : (
-        // <MazoDeCanciones />
-        <></>
+      <ImagenMandarina
+        mandarinaHeridaEsVisible={mandarinaHeridaEsVisible}
+        cuandoLaApreten={() => apretarLaMandarina((prev) => !prev)}
+      />
+      {apretoLaMandarina && (
+        <InputSecreto
+          hayError={hayError}
+          escribioLaCorrecta={escribioLaCorrecta}
+        />
       )}
       {/* <div className="youtube-video-contenedor">
       <iframe height="250" width="320" title="cancion-para-desayunar" 
---
+++ app/mazo-de-canciones/page.tsx
@@ -0,0 +1,5 @@
+function MazoDeCanciones() {
+  return <div className="">Canciones</div>;
+}
+
+export default MazoDeCanciones;
