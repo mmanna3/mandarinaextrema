75e5adca7961b43f09bb63521ee1b8adf72808d4
mmanna3 <mmmannna3@gmail.com>
2023-08-30 16:50:25 -0300

    Sube imágenes sin optimizar porque falla el deploy de next a la github page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: "export",  Lo comenté porque me tiró error, creo que tiene que ver con la optimización de imágenes. Ojalá vos tengas más tiempo/ganas que yo
+  // output: "export", //Lo comenté porque me tiró error, creo que tiene que ver con la optimización de imágenes. Ojalá vos tengas más tiempo/ganas que yo
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
