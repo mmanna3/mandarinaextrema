7e43498fb0bf7c484d02760d4aa70f74a4e1b4b2
mmanna3 <mmmannna3@gmail.com>
2025-01-24 21:32:26 -0300

    me gusta cómo se ve en mobile

--- app/components/body.tsx
+++ app/components/body.tsx
@@ -1,6 +1,7 @@
 "use client";
 import ImagenMandarina from "@/components/imagenMandarina";
 import { useState } from "react";
+import LinkInclinado from "./links";
 
 const Body = () => {
   const [mandarinaHeridaEsVisible, mostrarMandarinaHerida] = useState(false);
@@ -9,9 +10,51 @@ const Body = () => {
 
   return (
     <div className="flex flex-col items-center justify-center mt-36">
-      <ImagenMandarina
-        mandarinaHeridaEsVisible={mandarinaHeridaEsVisible}
-        cuandoLaApreten={() => {}}
+      <LinkInclinado
+        className="mr-48 rotate-[-10deg]"
+        href="/mazo-de-canciones"
+        text="las letras"
+      />
+      <LinkInclinado
+        className="ml-24 mt-8 rotate-[10deg]"
+        href="https://example.com"
+        text="instagram"
+      />
+      <LinkInclinado
+        className="mr-48 mt-8 rotate-[-25deg]"
+        href="https://example.com"
+        text="el disco"
+      />
+      <LinkInclinado
+        className="ml-40 mt-[-10px] rotate-[-15deg]"
+        href="https://example.com"
+        text="canciones"
+      />
+      <div className="my-8">
+        <ImagenMandarina
+          mandarinaHeridaEsVisible={mandarinaHeridaEsVisible}
+          cuandoLaApreten={() => {}}
+        />
+      </div>
+      <LinkInclinado
+        className="mr-48 rotate-[15deg]"
+        href="/blog"
+        text="escritos"
+      />
+      <LinkInclinado
+        className="ml-40 rotate-[-10deg]"
+        href="/covers"
+        text="covers"
+      />
+      <LinkInclinado
+        className="mr-32 mt-8 rotate-[10deg]"
+        href="https://example.com"
+        text="youtube"
+      />
+      <LinkInclinado
+        className="ml-40 mt-[10px] rotate-[-15deg]"
+        href="https://example.com"
+        text="spotify"
       />
     </div>
   );
